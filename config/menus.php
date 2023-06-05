6d449666bd3fa46159bbe5d39cdc9bf55d5b7f46
DanieleSpuria <daniele.spuria.jama@gmail.com>
2023-06-05 18:03:00 +0200

    add comics.php, menus.php

---
+++ config/menus.php
@@ -0,0 +1,227 @@
+<?php
+
+return [
+
+    'nav_menu' => [
+        [
+        'name'=> 'characters',
+        'href' => '#',
+        'active' => true
+        ],
+        [
+        'name'=> 'comics',
+        'href' => '#',
+        'active' => false
+        ],
+        [
+        'name'=> 'movie',
+        'href' => '#',
+        'active' => false
+        ],
+        [
+        'name'=> 'tv',
+        'href' => '#',
+        'active' => false
+        ],
+        [
+        'name'=> 'games',
+        'href' => '#',
+        'active' => false
+        ],
+        [
+        'name'=> 'collectibles',
+        'href' => '#',
+        'active' => false
+        ],
+        [
+        'name'=> 'videos',
+        'href' => '#',
+        'active' => false
+        ],
+        [
+        'name'=> 'fans',
+        'href' => '#',
+        'active' => false
+        ],
+        [
+        'name'=> 'news',
+        'href' => '#',
+        'active' => false
+        ],
+        [
+        'name'=> 'shop',
+        'href' => '#',
+        'active' => false
+        ],
+    ],
+
+    'shop' => [
+        [
+        'name'=> 'digital comics',
+        'img' => 'buy-comics-digital-comics.png',
+        'href' => '#'
+        ],
+        [
+        'name'=> 'dc merchandise',
+        'img' => 'buy-comics-merchandise.png',
+        'href' => '#'
+        ],
+        [
+        'name'=> 'subcription',
+        'img' => 'buy-comics-subscriptions.png',
+        'href' => '#'
+        ],
+        [
+        'name'=> 'comic shop locator',
+        'img' => 'buy-comics-shop-locator.png',
+        'href' => '#'
+        ],
+        [
+        'name'=> 'dc power visa',
+        'img' => 'buy-dc-power-visa.svg',
+        'href' => '#'
+        ],
+    ],
+
+    'dcComics' => [
+        [
+        'name'=> 'characters',
+        'href' => '#'
+        ],
+        [
+        'name'=> 'comics',
+        'href' => '#'
+        ],
+        [
+        'name'=> 'movie',
+        'href' => '#'
+        ],
+        [
+        'name'=> 'tv',
+        'href' => '#'
+        ],
+        [
+        'name'=> 'games',
+        'href' => '#'
+        ],
+        [
+        'name'=> 'videos',
+        'href' => '#',
+        'active' => false
+        ],
+        [
+        'name'=> 'fans',
+        'href' => '#'
+        ],
+        [
+        'name'=> 'news',
+        'href' => '#',
+        'active' => false
+        ]
+    ],
+
+    'footerShop' => [
+        [
+        'name'=> 'Shop DC',
+        'href' => '#'
+        ],
+        [
+        'name'=> 'Schop DC Collection',
+        'href' => '#'
+        ],
+    ],
+
+    'dC' => [
+    [
+        'name'=> 'Teams Of Use',
+        'href' => '#'
+    ],
+    [
+        'name'=> 'Privacy policy (News)',
+        'href' => '#'
+    ],
+    [
+        'name'=> 'Ad Choices',
+        'href' => '#'
+    ],
+    [
+        'name'=> 'Advertising',
+        'href' => '#'
+    ],
+    [
+        'name'=> 'Jobs',
+        'href' => '#'
+    ],
+    [
+        'name'=> 'Subscriptions',
+        'href' => '#'
+    ],
+    [
+        'name'=> 'Talent Workshops',
+        'href' => '#'
+    ],
+    [
+        'name'=> 'CPSC Certificates',
+        'href' => '#'
+    ],
+    [
+        'name'=> 'Ratings',
+        'href' => '#'
+    ],
+    [
+        'name'=> 'Shop Help',
+        'href' => '#'
+    ],
+    [
+        'name'=> 'Contact Us',
+        'href' => '#'
+    ],
+    ],
+
+    'sites' => [
+    [
+        'name'=> 'DC',
+        'href' => '#'
+    ],
+    [
+        'name'=> 'MAD Magazine',
+        'href' => '#'
+    ],
+    [
+        'name'=> 'DC Kids',
+        'href' => '#'
+    ],
+    [
+        'name'=> 'DC Universe',
+        'href' => '#'
+    ],
+    [
+        'name'=> 'DC Power VIsa',
+        'href' => '#'
+    ]
+    ],
+
+    'social' => [
+    [
+    'img' => 'footer-facebook.png',
+    'href' => '#'
+    ],
+    [
+    'img' => 'footer-periscope.png',
+    'href' => '#'
+    ],
+    [
+    'img' => 'footer-pinterest.png',
+    'href' => '#'
+    ],
+    [
+    'img' => 'footer-twitter.png',
+    'href' => '#'
+    ],
+    [
+    'img' => 'footer-youtube.png',
+    'href' => '#'
+    ]
+    ]
+
+];
